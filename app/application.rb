7b72d067a1bdee160de4bbc13eeb1a6f15af72ef
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-17 15:34:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,11 @@
 class Application
 
-def call(env)
-  resp =Rack::Response.new
-  #get time here: time = TBD
-  time = Time.new
+  def call(env)
+    resp =Rack::Response.new
+    #get time here: time = TBD
+    time = Time.new
 
-  #Write good afternoon or good Morning
-  # if time < resp.write
+    #Write good afternoon or good Morning
+    # if time < resp.write
   end
 end
