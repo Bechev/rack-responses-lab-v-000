e3269fc55c31f60bd371048504cba1da655e1a14
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-17 15:44:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     #get time here: time = TBD
     time = Time.new
-    resp.write "#{time.hour}"
+    resp.write "#{time}"
     #Write good afternoon or good Morning
     # if time < resp.write
     resp.finish
